9e1c185dfd21ca52b40e538d48ada27d727c9423
Mohith Vegi <fe438@NSLHUB.COM>
2021-03-24 21:16:40 +0530

    24 march

---
+++ DailyChallenge/2020/March/24.cpp
@@ -0,0 +1,37 @@
+//https://leetcode.com/explore/challenge/card/march-leetcoding-challenge-2021/591/week-4-march-22nd-march-28th/3683/
+
+// Time:  O(nlogn)
+// Space: O(n)
+
+class Solution {
+public:
+    vector<int> advantageCount(vector<int>& A, vector<int>& B) {
+        vector<int> sortedA(A.cbegin(), A.cend());
+        sort(sortedA.begin(), sortedA.end());
+        vector<int> sortedB(B.cbegin(), B.cend());
+        sort(sortedB.begin(), sortedB.end());
+
+        unordered_map<int, vector<int>> candidates;
+        vector<int> others;
+        int j = 0;
+        for (const auto& a : sortedA) {
+            if (a > sortedB[j]) {
+                candidates[sortedB[j]].emplace_back(a);
+                ++j;
+            } else {
+                others.emplace_back(a);
+            }
+        }
+        vector<int> result;
+        for (const auto& b : B) {
+            if (!candidates[b].empty()) {
+                result.emplace_back(candidates[b].back());
+                candidates[b].pop_back();
+            } else {
+                result.emplace_back(others.back());
+                others.pop_back();
+            }
+        }
+        return result;
+    }
+};
