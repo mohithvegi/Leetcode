f7af4c7c2cec7e753130a2244695f0bae21a441e
mohithvegi <mohithvegi@gmail.com>
2021-04-12 20:58:21 +0530

    12 april

---
+++ DailyChallenge/2020/April/12.java
@@ -0,0 +1,11 @@
+// https://leetcode.com/explore/challenge/card/april-leetcoding-challenge-2021/594/week-2-april-8th-april-14th/3705/
+
+
+class Solution {
+    public int[] constructArray(int n, int k) {
+        int[] res = new int[n];
+        for (int i = 0, l = 1, r = n; l <= r; i++)
+            res[i] = k > 1 ? (k-- % 2 != 0 ? l++ : r--) : (k % 2 != 0? l++ : r--);
+        return res;
+    }
+}
