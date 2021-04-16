d9d9120d0993131e02e4716e0fa077842887b2c3
mohithvegi <mohithvegi@gmail.com>
2021-04-16 17:33:56 +0530

    16th April

---
+++ DailyChallenge/2020/April/16.java
@@ -0,0 +1,37 @@
+// https://leetcode.com/explore/challenge/card/april-leetcoding-challenge-2021/595/week-3-april-15th-april-21st/3710/
+
+
+class Solution {
+    public String removeDuplicates(String s, int k) {
+        Stack<Pair> stack = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            if (!stack.isEmpty() && stack.peek().c == ch) {
+                stack.peek().freq++;
+            } else {
+                stack.push(new Pair(ch, 1));
+            }
+            if (stack.peek().freq == k) {
+                stack.pop();
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            Pair peek = stack.pop();
+            for (int i = 0; i < peek.freq; i++) {
+                sb.append(peek.c);
+            }
+        }
+        return sb.reverse().toString();
+    }
+
+    class Pair {
+        char c;
+        int freq;
+
+        public Pair(char c, int freq) {
+            this.c = c;
+            this.freq = freq;
+        }
+    }
+}
