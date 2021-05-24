08a2a30128acc253bf72e293e20233187d57efa9
mohithvegi <mohithvegi@gmail.com>
2021-05-24 21:47:33 +0530

    24 may

---
+++ DailyChallenge/2020/May/24.py
@@ -0,0 +1,5 @@
+# https://leetcode.com/explore/challenge/card/may-leetcoding-challenge-2021/601/week-4-may-22nd-may-28th/3754/
+
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        return s.lower()
