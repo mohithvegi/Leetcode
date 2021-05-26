0142a5a7ad1d49614be66929c2297df24ca55223
mohithvegi <mohithvegi@gmail.com>
2021-05-26 22:28:16 +0530

    26th may

---
+++ DailyChallenge/2020/May/26.py
@@ -0,0 +1,6 @@
+# https://leetcode.com/explore/challenge/card/may-leetcoding-challenge-2021/601/week-4-may-22nd-may-28th/3756/
+
+
+class Solution:
+    def minPartitions(self, n: str) -> int:
+        return max(n)
