c621e077b7a3b2889c92f1a6c0a20d5e7703aba2
mohithvegi <mohithvegi@gmail.com>
2021-06-09 10:23:41 +0530

    8th june

---
+++ DailyChallenge/2020/June/8.java
@@ -0,0 +1,51 @@
+// https://leetcode.com/explore/challenge/card/june-leetcoding-challenge-2021/604/week-2-june-8th-june-14th/3772/
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        int preStart = 0;
+    int preEnd = preorder.length-1;
+    int inStart = 0;
+    int inEnd = inorder.length-1;
+ 
+    return construct(preorder, preStart, preEnd, inorder, inStart, inEnd);
+    }
+    
+    public TreeNode construct(int[] preorder, int preStart, int preEnd, int[] inorder, int inStart, int inEnd){
+    if(preStart>preEnd||inStart>inEnd){
+        return null;
+    }
+ 
+    int val = preorder[preStart];
+    TreeNode p = new TreeNode(val);
+ 
+    //find parent element index from inorder
+    int k=0;
+    for(int i=0; i<inorder.length; i++){
+        if(val == inorder[i]){
+            k=i;
+            break;
+        }
+    }
+ 
+    p.left = construct(preorder, preStart+1, preStart+(k-inStart), inorder, inStart, k-1);
+    p.right= construct(preorder, preStart+(k-inStart)+1, preEnd, inorder, k+1 , inEnd);
+ 
+    return p;
+}
+}
