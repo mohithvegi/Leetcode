4446e34ad8d63df4fdd7df6f31d0f699dc402cd8
mohithvegi <mohithvegi@gmail.com>
2021-06-11 19:57:41 +0530

    11th june

---
+++ DailyChallenge/2020/June/11.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/explore/challenge/card/june-leetcoding-challenge-2021/604/week-2-june-8th-june-14th/3775/
+
+class Solution(object):
+    def stoneGameVII(self, stones):
+        """
+        :type stones: List[int]
+        :rtype: int
+        """
+        def score(i, j):
+            return prefix[j+1]-prefix[i]
+
+        prefix = [0]
+        for stone in stones:
+            prefix.append(prefix[-1]+stone)
+        dp = [[0 for _ in xrange(len(stones))] for _ in xrange(2)]
+        for i in reversed(xrange(len(stones))):
+            for j in xrange(i+1, len(stones)):
+                dp[i%2][j] = max(score(i+1, j)-dp[(i+1)%2][j], score(i, j-1)-dp[i%2][j-1])
+        return dp[0][-1]
